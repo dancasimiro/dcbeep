c12bdf75b910666812714f73731bd85dd81daf9c
Daniel C. Casimiro <daniel.casimiro@navy.mil>
2009-10-20 09:37:23 -0400

    Add a beep error category to the project.
    This allows me to create customized system error codes for application usage.

--- beep/reply-code.hpp
+++ beep/reply-code.hpp
@@ -4,6 +4,11 @@
 /// UNCLASSIFIED
 #ifndef BEEP_REPLY_CODE_HEAD
 #define BEEP_REPLY_CODE_HEAD 1
+
+#include <cassert>
+#include <string>
+#include <boost/system/error_code.hpp>
+
 namespace beep {
 namespace reply_code {
 
@@ -29,5 +34,64 @@ enum reply_code_type {
 typedef reply_code_type rc_enum;
 
 }      // namespace reply_code
+
+class error_category : public boost::system::error_category {
+public:
+	typedef std::string string;
+	virtual ~error_category() { }
+
+	virtual const char *name() const { return "BEEP Error Category"; }
+	virtual string message(int ev) const
+	{
+		using namespace beep::reply_code;
+		switch (ev) {
+		case service_not_available:
+			return string("BEEP service is not available");
+		case requested_action_not_taken:
+			return string("BEEP requested action was not taken");
+		case requested_action_aborted:
+			return string("BEEP requested action aborted");
+		case temporary_authentication_failure:
+			return string("BEEP temporary authentication failure");
+		case general_syntax_error:
+			return string("BEEP general syntax error (Badly formed XML)");
+		case syntax_error_in_parameters:
+			return string("BEEP syntax error in parameters (Bad XML)");
+		case parameter_not_implemented:
+			return string("BEEP parameter not implemented");
+		case authentication_required:
+			return string("BEEP authentication required");
+		case authentication_mechanism_insufficient:
+			return string("BEEP authentication mechanism is insufficient");
+		case authentication_failure:
+			return string("BEEP authentication failure");
+		case action_not_authorized_for_user:
+			return string("BEEP action is not authorized for this user");
+		case authentication_mechanism_requires_encryption:
+			return string("BEEP authentication mechanism requires encryption");
+		case requested_action_not_accepted:
+			return string("BEEP requested action was not accepted");
+		case parameter_invalid:
+			return string("BEEP parameter is invalid");
+		case transaction_failed:
+			return string("BEEP transaction failed");
+		case success:
+			return string("BEEP success code");
+		default:
+			assert(false);
+			return string("Unknown BEEP error");
+		}
+	}
+};
+
+inline
+const error_category & get_beep_category()
+{
+	static const error_category ecat;
+	return ecat;
+}
+
+static const error_category &beep_category = get_beep_category();
+
 }      // namespace beep
 #endif // BEEP_REPLY_CODE_HEAD
