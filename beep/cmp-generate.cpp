8d09a75d072c964e2bcc0c7e0c8eac352b0580e0
Daniel C. Casimiro <daniel.casimiro@navy.mil>
2010-08-16 15:42:21 -0400

    Implement start message generation.

--- beep/cmp-generate.cpp
+++ beep/cmp-generate.cpp
@@ -11,22 +11,22 @@
 #include "frame.hpp"
 
 #include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 
 namespace beep {
 namespace cmp {
 
 namespace karma = boost::spirit::karma;
+namespace phoenix = boost::phoenix;
 
 template <typename OutputIterator>
 struct output_greeting_grammar
 	: karma::grammar<OutputIterator, greeting_message()>
 {
 	output_greeting_grammar()
-		: output_greeting_grammar::base_type(xml)
+		: output_greeting_grammar::base_type(greeting_tag)
 	{
 		using karma::string;
-		using karma::uint_;
-		using namespace karma::labels;
 
 		profile_uri = "<profile uri=\"" << string << "\" />";
 
@@ -37,55 +37,61 @@ struct output_greeting_grammar
 			;
 
 		greeting_tag = server_greeting | empty_greeting;
-#if 0
-		profile_tag =
-			string("<profile ")
-			<< "uri=\""
-			<< string[bind(&profile_element::uri, _val)]
-			<< "\""
-			<< "/>"
-			;
-
-		start_channel_tag =
-			omit[string("<start")]
-			/*
-			<< "number=\""
-			<< uint_[_pass = (_1 < 2147483648u && _1 > 0), bind(&start_message::channel, _val)]
-			<< '\"'
-			<< ' '
-			<< "serverName=\""
-			<< string[bind(&start_message::server_name, _val)]
-			<< '\"'
-			<< '>'
-			<< +profile_tag[bind(&start_message::profiles, _val)]
-			*/
-			<< "</start>"
-			;
-#endif
-		xml =
-			greeting_tag
-			//| start_channel_tag
-			//| profile_tag
-			;
 	}
 	karma::rule<OutputIterator, std::string()> profile_uri;
 	karma::rule<OutputIterator, void()> empty_greeting;
 	karma::rule<OutputIterator, greeting_message()> server_greeting;
 	karma::rule<OutputIterator, greeting_message()> greeting_tag;
+};     // output_greeting_grammar
+
+template <typename OutputIterator>
+struct output_start_grammar
+	: karma::grammar<OutputIterator, start_message()>
+{
+	output_start_grammar()
+		: output_start_grammar::base_type(start_channel_tag)
+	{
+		using karma::string;
+		using karma::uint_;
+		using namespace karma::labels;
+		using karma::_1;
+
+		profile_tag =
+			string("<profile ")
+			<< "uri=\""
+			<< string[_1 = bind(&profile_element::uri, _val)]
+			<< "\""
+			<< " />"
+			;
+
+		start_channel_tag =
+			string("<start ")
+			<< "number=\""
+			<< uint_[_1 = bind(&start_message::channel, _val), _pass = (_1 < 2147483648u && _1 > 0)]
+			<< '\"'
+			<< ' '
+			<< "serverName=\""
+			<< string[_1 = bind(&start_message::server_name, _val)]
+			<< '\"'
+			<< '>'
+			<< (+profile_tag)[_1 = bind(&start_message::profiles, _val)]
+			<< "</start>"
+			;
+	}
 	karma::rule<OutputIterator, profile_element()> profile_tag;
 	karma::rule<OutputIterator, start_message()> start_channel_tag;
-	karma::rule<OutputIterator, greeting_message()> xml;
-};     // output_protocol_grammar
+	//karma::rule<OutputIterator, close_message()> close_channel_tag;
+	//karma::rule<OutputIterator, ok_message()> ok_tag;
+	//karma::rule<OutputIterator, error_message()> error_tag;
+};     // output_start_grammar
 
 namespace detail {
-template <typename T>
-std::string do_generate(const T &in)
+template <class Generator, typename T>
+std::string do_generate(const Generator &generator, const T &in)
 {
 	std::string generated;
-	output_greeting_grammar<std::back_insert_iterator<std::string> > my_grammar;
 	std::back_insert_iterator<std::string> sink(generated);
-	const bool result = karma::generate(sink, my_grammar, in);
-	if (!result) {
+	if (!karma::generate(sink, generator, in)) {
 		throw std::runtime_error("bad generation!");
 	}
 	return generated;
@@ -98,7 +104,8 @@ public:
 		message msg;
 		msg.set_mime(mime::beep_xml());
 		msg.set_type(RPY);
-		msg.set_content(do_generate(greeting));
+		const output_greeting_grammar<std::back_insert_iterator<std::string> > grammar;
+		msg.set_content(do_generate(grammar, greeting));
 		return msg;
 	}
 
@@ -107,11 +114,12 @@ public:
 		message msg;
 		msg.set_mime(mime::beep_xml());
 		msg.set_type(MSG);
-		//msg.set_content(do_generate(start));
+		const output_start_grammar<std::back_insert_iterator<std::string> > grammar;
+		msg.set_content(do_generate(grammar, start));
 		return msg;
 	}
 
-	message operator()(const close_message &close) const
+	message operator()(const close_message &/*close*/) const
 	{
 		message msg;
 		msg.set_mime(mime::beep_xml());
@@ -120,7 +128,7 @@ public:
 		return msg;
 	}
 
-	message operator()(const ok_message &ok) const
+	message operator()(const ok_message &/*ok*/) const
 	{
 		message msg;
 		msg.set_mime(mime::beep_xml());
@@ -129,7 +137,7 @@ public:
 		return msg;
 	}
 
-	message operator()(const error_message &error) const
+	message operator()(const error_message &/*error*/) const
 	{
 		message msg;
 		msg.set_mime(mime::beep_xml());
@@ -138,7 +146,7 @@ public:
 		return msg;
 	}
 
-	message operator()(const profile_element &profile) const
+	message operator()(const profile_element &/*profile*/) const
 	{
 		message msg;
 		msg.set_mime(mime::beep_xml());
