70c745556a067f42992dce14a33004712cf03bc8
Daniel C. Casimiro <daniel.casimiro@navy.mil>
2010-08-17 10:57:24 -0400

    Remove the adaptation of the close_message.

--- beep/cmp-adapt.hpp
+++ beep/cmp-adapt.hpp
@@ -9,13 +9,6 @@ BOOST_FUSION_ADAPT_STRUCT(beep::cmp::greeting_message,
 						  (std::vector<std::string>, profile_uris)
 						 )
 
-BOOST_FUSION_ADAPT_STRUCT(beep::cmp::close_message,
-						  (unsigned int, channel)
-						  (unsigned int, code)
-						  (std::string, language)
-						  (std::string, diagnostic)
-						  )
-
 BOOST_FUSION_ADAPT_STRUCT(beep::cmp::error_message,
 						  (unsigned int, code)
 						  (std::string, language)
